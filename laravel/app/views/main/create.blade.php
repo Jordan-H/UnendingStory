fcdfee169858df1ef555cf5247d2b96e2b7b30e6
Jason Cheung <jayson.h.cheung@gmail.com>
2016-04-14 12:42:44 -0700

    Minor changes to css and how data is displayed.

--- laravel/app/views/main/create.blade.php
+++ laravel/app/views/main/create.blade.php
@@ -5,31 +5,42 @@ Author: Jason Cheung
 
 @section('pagetitle')
     <title>Home Page</title>
-
 @stop
 
 @section('headers')
-{{-- HTML::style('css/Reset.css') --}}
- {{ HTML::style('css/main.css') }}
-    <link href='https://fonts.googleapis.com/css?family=Montserrat|Lora|Tangerine' rel='stylesheet' type='text/css'>
+    {{-- HTML::style('css/Reset.css') --}}
+    {{ HTML::style('css/main.css') }}
+    {{ HTML::style('https://fonts.googleapis.com/css?family=Montserrat|Lora|Tangerine') }}
 @stop
 
 @section('maincontent')
     <div id="main-content">
-        <h1>An Unending Story</h1>
+        <div id="head">
+            <h1>An Unending Story</h1>
+        </div>
         <div id="content">
             <p>
             @foreach ($posts as $post)
-                {{ $post }}
+                @if (strpos($post, '.') !== FALSE)
+                    {{ $post . "</p><p>" }}
+                @else
+                    {{ $post }}
+                @endif
             @endforeach
             </p>
+            <br>
         </div>
-        {{ Form::open(['action' => 'MainController@store']) }}
-        <div id="submit-post">
-            {{ Form::label('post', 'Continue the story: ') }}
-            {{ Form::text('post') }}
-        </div>
-        {{Form::submit('Submit')}}
-        {{ Form::close() }}
     </div>
+
+    {{ Form::open(['action' => 'MainController@store']) }}
+    <div id="submit-post">
+        {{ Form::label('post', 'Continue the story: ') }}
+        {{ Form::text('post') }}
+        {{Form::submit('Submit')}}
+    </div>
+    {{ Form::close() }}
+
+    <script type="text/javascript">
+        window.scrollTo(0,document.body.scrollHeight);
+    </script>
 @stop
