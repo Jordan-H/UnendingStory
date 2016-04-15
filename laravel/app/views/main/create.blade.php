20b791c81ea89070b562a8e46bf4b71581f13456
Jason Cheung <jayson.cheung@hotmail.com>
2016-04-14 18:14:49 -0700

    Styling
    No idea what to do.

--- laravel/app/views/main/create.blade.php
+++ laravel/app/views/main/create.blade.php
@@ -14,33 +14,56 @@ Author: Jason Cheung
 @stop
 
 @section('maincontent')
+
+    <div id="social-media">
+        <a href="mailto:unendingstory@gmail.com" class="icons" style="color: black">&#xe010;</a>
+        <a href="https://twitter.com/Unending_Story" class="icons" style="color: red">&#xe096;</a>
+        <a href="https://www.facebook.com/people/Unending-Story/100011687526998" class="icons" style="color: #3B5998">&#xe093;</a>
+    </div>
+
     <div id="main-content">
         <div id="head">
             <h1>An Unending Story</h1>
         </div>
         <div id="content">
+            <h3>Day 1</h3><!-- maybe we could have something like this -->
             <p>
-            @foreach ($posts as $post)
-                @if (strpos($post, '.') !== FALSE)
-                    {{ $post . "</p><p>" }}
-                @else
+                @foreach ($posts as $post)
                     {{ $post }}
-                @endif
-            @endforeach
+                    @if (strpos($post, '.') !== FALSE)
+                        </p><p>
+                    @endif
+                @endforeach
             </p>
             <br>
         </div>
     </div>
 
-    {{ Form::open(['action' => 'MainController@store']) }}
+    <a href="http://twitter.com/minimalmonkey" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+    <a href="http://facebook.com" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+    <a href="http://plus.google.com" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+
     <div id="submit-post">
+        {{ Form::open(['action' => 'MainController@store']) }}
+
         {{ Form::label('post', 'Continue the story: ') }}
         {{ Form::text('post') }}
-        {{Form::submit('Submit')}}
+        {{ Form::submit('Submit') }}
+        {{ Form::close() }}
     </div>
-    {{ Form::close() }}
+
+
 
     <script type="text/javascript">
         window.scrollTo(0,document.body.scrollHeight);
     </script>
 @stop
+
+
+{{--
+@if (strpos($post, '.') !== FALSE)
+    {{ $post . "</p><p>" }}
+@else
+    {{ $post }}
+@endif
+--}}
